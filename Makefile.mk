0ce23873484ec928992306e62209b1520be8071a
Mangoberry Kivocado Plumango Grapaya <v1taan0nyma@gmail.com>
2026-03-16 17:58:48 +1100

    Update Makefile.mk

--- Makefile.mk
+++ Makefile.mk
@@ -1,19 +1,24 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
+# Object files for ifdtool
 ifdtoolobj = ifdtool.o fmap.o kv_pair.o valstr.o
 
+# Relative paths: no hardcoded $(top)
 IFDTOOLCFLAGS  = -O2 -g -Wall -Wextra -Wmissing-prototypes -Werror
-IFDTOOLCFLAGS += -I$(top)/src/commonlib/include -I$(top)/src/commonlib/bsd/include
-IFDTOOLCFLAGS += -I$(top)/util/cbfstool/flashmap
-IFDTOOLCFLAGS += -include $(top)/src/commonlib/bsd/include/commonlib/bsd/compiler.h
+IFDTOOLCFLAGS += -I ./commonlib/include -I ./commonlib/bsd/include
+IFDTOOLCFLAGS += -I ./flashmap
+IFDTOOLCFLAGS += -include ./commonlib/bsd/include/commonlib/bsd/compiler.h
 IFDTOOLCFLAGS += -D_DEFAULT_SOURCE # for endianness converting functions
 
-$(objutil)/ifdtool/%.o: $(top)/util/ifdtool/%.c
+# Compile ifdtool sources
+$(objutil)/ifdtool/%.o: ./ifdtool/%.c
 	$(HOSTCC) $(IFDTOOLCFLAGS) $(HOSTCFLAGS) -c -o $@ $<
 
-$(objutil)/ifdtool/%.o: $(top)/util/cbfstool/flashmap/%.c
+# Compile flashmap sources
+$(objutil)/ifdtool/%.o: ./flashmap/%.c
 	$(HOSTCC) $(IFDTOOLCFLAGS) $(HOSTCFLAGS) -c -o $@ $<
 
+# Link ifdtool
 $(objutil)/ifdtool/ifdtool: $(addprefix $(objutil)/ifdtool/,$(ifdtoolobj))
-	printf "   IFDTOOL\n"
+	@printf "   IFDTOOL\n"
 	$(HOSTCC) ${LDFLAGS} $(addprefix $(objutil)/ifdtool/,$(ifdtoolobj)) -o $@
